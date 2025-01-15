2a79f41f6694c113e3898addc6b6e6d41eb28009
Aman Kumar <amankarguwal0@gmail.com>
2025-01-15 21:36:38 +0530

    Add messageSchema for content validation using zod

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const contentValidation = z
+  .string()
+  .min(10, { message: "Message must be at least 10 characters long" })
+  .max(300, { message: "Message must be at most 300 characters long" });
+
+export const messageSchema = z.object({
+    content: contentValidation,
+});
