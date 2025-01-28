ef4336e8ff992f8368202a20bc251cff1a2f01ed
Aman Kumar <amankarguwal0@gmail.com>
2025-01-28 05:36:31 +0530

    Add VerificationEmail component for user registration verification

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Font,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+
+interface VerificationEmailProps {
+  username: string;
+  otp: string;
+}
+
+export default function VerificationEmail({
+  username,
+  otp,
+}: VerificationEmailProps) {
+  return (
+    <Html lang="en" dir="ltr">
+      <Head>
+        <title>
+          Verification Code
+          <Font
+            fontFamily="Roboto"
+            webFont={{
+              url: "https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2",
+              format: "woff2",
+            }}
+            fontWeight={400}
+            fontStyle="normal"
+            fallbackFontFamily={"sans-serif"}
+          />
+        </title>
+      </Head>
+      <Preview>Here&apos;s your verification code: {otp}</Preview>
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {username},</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for signing up with us. Please use the following
+            verification code to complete your registration.
+          </Text>
+        </Row>
+        <Row>
+          <Text>Verification code: {otp}</Text>
+        </Row>
+        <Row>
+          <Button
+            href="https://localhost:3000/verify?username={username}&otp={otp}"
+            style={{ color: "#61dafb" }}
+          >
+            Verify Here
+          </Button>
+        </Row>
+      </Section>
+    </Html>
+  );
+}
