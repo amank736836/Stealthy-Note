b2dc6830c436f45c044b60f5ebeb4b5969bf9f0f
Aman Kumar <amankarguwal0@gmail.com>
2025-02-03 21:58:06 +0530

    Add POST route for user verification with code validation

---
+++ src/app/api/verify/route.ts
@@ -0,0 +1,100 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+
+    if (!username) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is required",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!code) {
+      return Response.json(
+        {
+          success: false,
+          message: "Code is required",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({
+      username: decodedUsername,
+    });
+
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid Username",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "User Verified Successfully",
+        },
+        {
+          status: 200,
+        }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "Verification Code has expired, please signup again to get a new code",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid Code",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("ErroR Verifying User", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error Verifying User",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
