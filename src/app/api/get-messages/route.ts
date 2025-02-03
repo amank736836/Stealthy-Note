0644a68f4440005a8830170b614b7949fe46177a
Aman Kumar <amankarguwal0@gmail.com>
2025-02-03 22:55:02 +0530

    Add GET API route for retrieving user messages with authorization check

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,68 @@
+import dbConnect from "@/lib/dbConnect";
+import { User } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import getServerSession from "next-auth";
+import UserModel from "@/model/User";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authorized",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const user: User = session?.user as User;
+
+  const userId = new mongoose.Types.ObjectId(user.id);
+
+  try {
+    const user = await UserModel.aggregate([
+      {
+        $match: {
+          id: userId,
+        },
+      },
+      {
+        $unwind: "$messages",
+      },
+      {
+        $sort: { "messages.createdAt": -1 },
+      },
+      {
+        $group: {
+          _id: "$_id",
+          messages: { $push: "$messages" },
+        },
+      },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return Response.json({
+      success: true,
+      data: user[0].messages,
+    });
+
+    
+  } catch (error) {}
+}
