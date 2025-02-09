925e2ba35003f6f5a98045dcc5dbdfb321287afe
Aman Kumar <amankarguwal0@gmail.com>
2025-02-09 14:41:46 +0530

    Refactor import path for signInSchema to improve module organization

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/hooks/use-toast";
-import { signInSchema } from "@/schemas/signInSchema";
+import { signInSchema } from "@/backend/schemas/signInSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
 import { signIn } from "next-auth/react";
