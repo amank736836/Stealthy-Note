780b220d68e36ef4df53b0c103e826a7fb9e897b
Aman Kumar <amankarguwal0@gmail.com>
2025-02-15 14:13:14 +0530

    Refactor Navbar component to improve layout and add theme toggle functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,35 +4,44 @@ import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { User } from "next-auth";
 import { Button } from "./ui/button";
+import ThemeToggle from "./ThemeToggle";
 
 function Navbar() {
   const { data: session } = useSession();
-
   const user: User = session?.user as User;
 
   return (
-    <nav className="p-4 md:p-6 shadow-md">
+    <nav className="p-4 md:p-6 shadow-md bg-white dark:bg-gray-900 text-black dark:text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <a href="/" className="text-xl font-bold mb-4 md:mb-0">
           Stealth Note
         </a>
+
         {session ? (
-          <>
-            <span className="mr-4">Welcome, {user.username || user.email}</span>
+          <div className="flex items-center space-x-4">
+            <span>Welcome, {user.username || user.email}</span>
+            <ThemeToggle />
+
             <Button
-              className="w-full  md:w-auto bg-slate-100 text-black"
+              className="w-full md:w-auto bg-slate-100 dark:bg-gray-800 text-black dark:text-white"
               variant="outline"
               onClick={() => signOut()}
             >
               Logout
             </Button>
-          </>
+          </div>
         ) : (
-          <Link href="/sign-in">
-            <Button className="w-full md:w-auto bg-slate-100 text-black">
-              Login
-            </Button>
-          </Link>
+          <div
+          className="flex items-center space-x-4"
+          >
+            <ThemeToggle />
+
+            <Link href="/sign-in">
+              <Button className="w-full md:w-auto bg-slate-100 dark:bg-gray-800 text-black dark:text-white">
+                Login
+              </Button>
+            </Link>
+          </div>
         )}
       </div>
     </nav>
