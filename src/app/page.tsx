73b93f146c5c866cdc6b8374f929f0fd9e6186b8
Aman Kumar <amankarguwal0@gmail.com>
2025-02-15 19:28:31 +0530

    Update references from "Stealth Note" to "Stealthy Note" in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,10 @@ const Home = () => {
             className="text-xl md:text-5xl font-bold lg:text-3xl
           xl:text-4xl"
           >
-            Dive into the world of Anonymous Conversations with Stealth Note 🕵️‍♂️
+            Dive into the world of Anonymous Conversations with Stealthy Note 🕵️‍♂️
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
-            Explore Stealth Note - Where your identity always remains a secret.
+            Explore Stealthy Note - Where your identity always remains a secret.
           </p>
         </section>
         <Carousel
@@ -90,7 +90,7 @@ const Home = () => {
         </Carousel>
       </main>
       <footer className="text-center p-2 md:p-2">
-        © 2025 Stealth Note. All rights reserved.
+        © 2025 Stealthy Note. All rights reserved.
       </footer>
     </>
   );
