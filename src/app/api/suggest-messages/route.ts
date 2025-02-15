84e412fb09b399b6e7b5322842c0dc85d104a5b7
Aman Kumar <amankarguwal0@gmail.com>
2025-02-15 14:12:46 +0530

    Refactor POST route in suggest-messages API to enhance question generation logic and error handling

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,49 +1,56 @@
 import { google } from "@ai-sdk/google";
 import { streamText } from "ai";
-import { createStreamableValue } from "ai/rsc";
 import { NextResponse } from "next/server";
-import OpenAI from "openai";
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
   try {
-    const prompt =
-      "Create a list of three open-ended and engaging questions formatted as single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing,foster curiosity, and contribute to a positive and welcoming conversational environment.";
+    const { exclude } = await req.json();
 
-    const stream = createStreamableValue("");
+    console.log("Excluding:", exclude);
 
-    (async () => {
-      const { textStream } = streamText({
-        model: google("gemini-1.5-flash-8b-latest"),
-        prompt: prompt,
-      });
+    const prompt = `Create a list of three open-ended and engaging questions formatted as a single string.
+    Each question should be separated by '||'. The questions are for an anonymous social messaging platform,
+    and should be suitable for a diverse audience. Avoid personal or sensitive topics.
+    Ensure the questions are intriguing and foster curiosity.
+    DO NOT INCLUDE any of the following questions: ${exclude || "None"}.
+     Each question MUST be at most 100 characters long.`;
 
-      for await (const delta of textStream) {
-        stream.update(delta);
-      }
+    const { textStream } = await streamText({
+      model: google("gemini-1.5-flash-8b-latest"),
+      prompt: prompt,
+      maxRetries: 3,
+    });
 
-      stream.done();
-    })();
-
-    return { output: stream.value };
-  } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = error;
-
+    if (!textStream) {
       return NextResponse.json(
         {
-          name,
-          status,
-          headers,
-          message,
+          success: false,
+          message: "Failed to generate questions. Please try again.",
         },
-        {
-          status,
-        }
+        { status: 500 }
       );
-    } else {
-      console.error("An Unexpected Error Occurred", error);
     }
+
+    let result = "";
+    for await (const delta of textStream) {
+      result += delta;
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: result,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("An Unexpected Error Occurred", error);
+
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
