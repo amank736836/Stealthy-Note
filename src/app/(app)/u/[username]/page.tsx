553be1cd7c932073b0a98276803987c5b7c5c59f
Aman Kumar <amankarguwal0@gmail.com>
2025-02-15 14:10:33 +0530

    Refactor SendMessage component for improved structure and user experience

--- src/app/(app)/u/[username]/page.tsx
+++ src/app/(app)/u/[username]/page.tsx
@@ -20,38 +20,26 @@ import axios, { AxiosError } from "axios";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
 const specialChar = "||";
 
 const parseStringMessages = (messageString: string): string[] => {
-  return messageString.split(specialChar);
+  return messageString.split(specialChar).map((msg) => msg.trim());
 };
 
 const initialMessageString =
   "What's your favorite movie?||Do you have any pets?||What's your dream job?";
 
 export default function SendMessage() {
-  const params = useParams<{
-    username: string;
-  }>();
-
+  const params = useParams<{ username: string }>();
   const username = params.username;
-
-  const {
-    completion: messageString,
-    setCompletion: setMessageString,
-    isLoading: isCompletionLoading,
-    error: completionError,
-  } = useCompletion({
-    api: "/api/suggest-messages",
-    initialCompletion: initialMessageString,
-  });
 
   const form = useForm<z.infer<typeof messageSchema>>({
     resolver: zodResolver(messageSchema),
+    defaultValues: { content: "" },
   });
 
   const messageContent = form.watch("content");
@@ -66,16 +54,13 @@ export default function SendMessage() {
     setIsLoading(true);
 
     try {
-      const response = await axios.post<ApiResponse>("../api/send-message", {
+      const response = await axios.post<ApiResponse>("/api/send-message", {
         ...data,
         username,
       });
 
       if (response.data.success) {
-        form.reset({
-          ...form.getValues(),
-          content: "",
-        });
+        form.reset({ content: "" });
       }
 
       toast({
@@ -86,12 +71,10 @@ export default function SendMessage() {
       });
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
-
       toast({
         title: "Error sending message",
         description:
-          axiosError.response?.data.message ??
-          `An error occurred while sending the message, ${error}`,
+          axiosError.response?.data.message ?? `An error occurred: ${error}`,
         variant: "destructive",
       });
     } finally {
@@ -99,103 +82,135 @@ export default function SendMessage() {
     }
   };
 
+  const {
+    completion: messageString,
+    setCompletion: setMessageString,
+    isLoading: isCompletionLoading,
+    error: completionError,
+  } = useCompletion({
+    api: "/api/suggest-messages",
+    initialCompletion: initialMessageString,
+  });
+
   const fetchSuggestedMessages = async () => {
     try {
-      setMessageString("");
+      const response = await axios.post<ApiResponse>("/api/suggest-messages", {
+        exclude: messageString,
+      });
+
+      if (response.data.success) {
+        setMessageString(response.data.message);
+      } else {
+        toast({
+          title: "Error fetching suggested messages",
+          description: response.data.message,
+          variant: "destructive",
+        });
+      }
     } catch (error) {
-      console.error("Failed to fetch suggested messages", error);
+      console.error("Failed to fetch suggested messages:", error);
     }
   };
 
   return (
-    <div className="container mx-auto my-8 p-4 bg-white rounded max-w-4xl">
-      <h1 className="text-4xl font-bold mb-6 text-center">
+    <div className="container mx-auto my-8 p-6 bg-white dark:bg-gray-900 rounded-lg max-w-4xl shadow-lg">
+      <h1 className="text-3xl font-bold text-center mb-6 text-gray-900 dark:text-white">
         Public Profile Link
       </h1>
+
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
           <FormField
             control={form.control}
             name="content"
             render={({ field }) => (
-              <FormItem
-              className="flex flex-col"
-              >
-                <FormLabel>Send Anonymous Message to @{username}</FormLabel>
+              <FormItem>
+                <FormLabel className="text-gray-900 dark:text-gray-300">
+                  Send Anonymous Message to @{username}
+                </FormLabel>
                 <FormControl>
                   <textarea
                     {...field}
-                    className="resize-none"
+                    className="resize-none p-2 w-full border rounded-md dark:bg-gray-700 dark:border-gray-600 dark:text-white"
                     placeholder="Write your anonymous message here"
                   />
                 </FormControl>
-                {/* <FormMessage>
-                  {form.formState.errors.content?.message}
-                </FormMessage> */}
                 <FormMessage />
               </FormItem>
             )}
           />
+
           <div className="flex justify-center">
-            {isLoading ? (
-              <Button disabled>
-                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                Please Wait
-              </Button>
-            ) : (
-              <Button type="submit" disabled={isLoading || !messageContent}>
-                Send Message
-              </Button>
-            )}
+            <Button
+              type="submit"
+              disabled={isLoading || !messageContent}
+              className="dark:bg-blue-500 dark:text-white"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please Wait
+                </>
+              ) : (
+                "Send Message"
+              )}
+            </Button>
           </div>
         </form>
       </Form>
 
-      <div className="space-y-4 my-8">
-        <div className="space-y-2">
-          <Button
-            onClick={fetchSuggestedMessages}
-            className="my-4"
-            disabled={isCompletionLoading}
-          >
-            {isCompletionLoading ? "Loading..." : "Suggest Messages"}
-          </Button>
-          {completionError && (
-            <p className="text-red-500">{completionError.message}</p>
-          )}
+      <div className="space-y-6 my-8">
+        <Button
+          onClick={fetchSuggestedMessages}
+          className="w-full dark:bg-gray-700 dark:text-white"
+          disabled={isCompletionLoading}
+        >
+          {isCompletionLoading ? "Loading..." : "Suggest Messages"}
+        </Button>
 
-          <p>Click on the any message below to select it.</p>
-        </div>
+        {completionError && (
+          <p className="text-red-500 text-center">{completionError.message}</p>
+        )}
 
-        <Card>
+        <Card className="dark:bg-gray-800 dark:border-gray-700">
           <CardHeader>
-            <h3 className="text-4xl font-semibold">Messages</h3>
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+              Suggested Messages
+            </h3>
           </CardHeader>
-          <CardContent className="flex flex-col space-y-4">
+          <CardContent className="flex flex-col space-y-3">
             {completionError ? (
               <p className="text-red-500">{completionError.message}</p>
             ) : messageString ? (
               parseStringMessages(messageString).map((message, index) => (
                 <Button
                   key={index}
-                  variant={"outline"}
+                  variant="outline"
                   onClick={() => handleMessageClick(message)}
-                  className="mb-2"
+                  className="w-full text-left break-words whitespace-pre-wrap p-3 my-2 border border-gray-300 dark:border-gray-600 rounded-md dark:text-white"
                 >
                   {message}
                 </Button>
               ))
             ) : (
-              <p>No messages to suggest</p>
+              <p className="text-gray-500 dark:text-gray-400">
+                No messages to suggest
+              </p>
             )}
           </CardContent>
         </Card>
       </div>
-      <Separator className="my-6" />
+
+      <Separator className="my-6 dark:bg-gray-700" />
+
       <div className="text-center">
-        <div className="mb-4">Get Your Own Message Board</div>
+        <p className="mb-4 text-gray-700 dark:text-gray-300">
+          Get Your Own Message Board
+        </p>
         <Link href="/sign-up">
-          <Button>Create Your Account</Button>
+          <Button className="dark:bg-blue-500 dark:text-white">
+            Create Your Account
+          </Button>
         </Link>
       </div>
     </div>
