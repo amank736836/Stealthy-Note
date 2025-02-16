1276111b50c86f55a89490ae1a6372bc5d68d2b4
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 14:27:50 +0530

    Fix max length validation message for identifier in signInSchema

--- src/backend/schemas/signInSchema.ts
+++ src/backend/schemas/signInSchema.ts
@@ -4,7 +4,7 @@ import { passwordValidation } from "./signUpSchema";
 export const identifierValidation = z
   .string()
   .min(2, { message: "Identifier must be at least 2 characters long" })
-  .max(50, { message: "Identifier must be at most 20 characters long" });
+  .max(50, { message: "Identifier must be at most 50 characters long" });
 
 export const signInSchema = z.object({
   identifier: identifierValidation,
