706d23023fd09db383fe9d2f1984ba1987343794
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 17:17:28 +0530

    Implement token-based authentication middleware with redirect logic for protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,28 @@
-export { auth as middleware } from "@/app/api/auth/[...nextauth]/option";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+  const url = req.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  runtime: "nodejs",
+  matcher: ["/verify/:path*", "/dashboard/:path*", "/", "/sign-up", "/sign-in"],
 };
