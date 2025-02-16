ce63aa8ba51baeff061c27df6ca1ccde793f25ca
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 18:30:43 +0530

    Refactor middleware to use JWT for authentication and redirect unauthenticated users from sign-in, sign-up, and verification pages to the dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,96 +1,26 @@
-import dbConnect from "@/backend/lib/dbConnect";
-import UserModel from "@/backend/model/User";
-import bcrypt from "bcryptjs";
-import NextAuth, { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-const authOptions: NextAuthConfig = {
-  providers: [
-    Credentials({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        identifier: {
-          label: "Username or Email",
-          type: "text",
-          placeholder: "Username or Email",
-        },
-        password: {
-          label: "Password",
-          type: "text",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials): Promise<any> {
-        await dbConnect();
-        try {
-          const user = await UserModel.findOne({
-            $or: [
-              { email: credentials.identifier },
-              { username: credentials.identifier },
-            ],
-          });
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
 
-          if (!user) {
-            throw new Error("No user found with this username or email");
-          }
+  console.log("token", token);
 
-          if (!user.isVerified) {
-            if (user.verifyCodeExpiry < new Date()) {
-              throw new Error(
-                "Verification code expired. Please request a new one"
-              );
-            } else {
-              throw new Error("Please verify your account before logging in");
-            }
-          }
+  const url = req.nextUrl;
 
-          const isValid = await bcrypt.compare(
-            credentials.password as string,
-            user.password
-          );
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
 
-          if (!isValid) {
-            throw new Error("Invalid password");
-          }
+  return NextResponse.next();
+}
 
-          return user;
-        } catch (error) {
-          throw new Error(`Error in authorize: ${error}`);
-        }
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      console.log("user", user);
-
-      if (user) {
-        token._id = user._id.toString();
-        token.isVerified = user.isVerified;
-        token.isAcceptingMessages = user.isAcceptingMessages;
-        token.username = user.username;
-      }
-
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user._id = token._id as string;
-        session.user.isVerified = token.isVerified as boolean;
-        session.user.isAcceptingMessages = token.isAcceptingMessages as boolean;
-        session.user.username = token.username as string;
-      }
-      return session;
-    },
-  },
-  pages: {
-    signIn: "/sign-in",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.AUTH_SECRET,
+export const config = {
+  matcher: ["/verify/:path*", "/dashboard/:path*", "/", "/sign-up", "/sign-in"],
 };
-
-export const { handlers, signIn, signOut, auth } = NextAuth(authOptions);
