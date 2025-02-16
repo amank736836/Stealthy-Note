057baa5c07d4983fd63dbe18b91450de296662d7
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 16:57:29 +0530

    Refactor middleware to use NextAuth for authentication and update path matching logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,14 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-
-  const url = req.nextUrl;
-
-  if (
-    token &&
-    (url.pathname.startsWith("/sign-in") ||
-      url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname === "/")
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-
-  return NextResponse.next();
-}
+export { auth as middleware } from '@/app/api/auth/[...nextauth]/option';
 
 export const config = {
-  matcher: ["/verify/:path*", "/dashboard/:path*", "/", "/sign-up", "/sign-in"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 };
