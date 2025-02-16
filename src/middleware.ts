52fe76f1ed6255c8850168acf3ef1be30c28d178
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 18:45:42 +0530

    Update token retrieval in middleware to specify cookie name for improved security

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,11 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    cookieName: "__Secure-authjs.session-token",
+  });
 
   console.log("token", token);
 
