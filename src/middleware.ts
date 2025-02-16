4241c8f4bec6998ccb5e82f53397d22d7f11fdbf
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 17:15:26 +0530

    Refactor middleware to use NextAuth options and update runtime configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,5 @@
-import { getToken } from "next-auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-
-  const url = req.nextUrl;
-
-  if (
-    token &&
-    (url.pathname.startsWith("/sign-in") ||
-      url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname === "/")
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-
-  return NextResponse.next();
-}
+export { auth as middleware } from "@/app/api/auth/[...nextauth]/option";
 
 export const config = {
-  matcher: ["/verify/:path*", "/dashboard/:path*", "/", "/sign-up", "/sign-in"],
+  runtime: "nodejs",
 };
