a99fc493c2142b3670ae8ada95878d9fc94690bb
Aman Kumar <amankarguwal0@gmail.com>
2025-02-16 17:18:17 +0530

    Remove redundant token validation and redirect logic from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,6 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
-
-  const url = req.nextUrl;
-
-  if (
-    token &&
-    (url.pathname.startsWith("/sign-in") ||
-      url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname === "/")
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
-
-  if (!token && url.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/sign-in", req.url));
-  }
-
   return NextResponse.next();
 }
 
