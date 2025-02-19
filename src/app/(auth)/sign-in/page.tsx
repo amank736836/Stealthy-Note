6566a2807bf7d1a81d3f3e932dccfa28a97e50c8
Aman Kumar <amankarguwal0@gmail.com>
2025-02-19 23:27:30 +0530

    Wrap SignIn component in Suspense for improved loading handling

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -17,7 +17,7 @@ import { Loader2 } from "lucide-react";
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
@@ -46,7 +46,6 @@ function SignIn() {
     }
   }, [identifier, form]);
 
-
   const onSubmit = async (data: z.infer<typeof signInSchema>) => {
     setIsSubmitting(true);
     const result = await signIn("credentials", {
@@ -73,114 +72,116 @@ function SignIn() {
   };
 
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-92px)] bg-gray-100 dark:bg-gray-900">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white dark:bg-gray-800 rounded-lg shadow-md">
-        <div className="text-center">
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-gray-900 dark:text-white">
-            Join Stealthy Note 🥷📝
-          </h1>
-          <p className="mb-6 text-gray-700 dark:text-gray-300">
-            Sign In to start stealing notes from your friends
-          </p>
-        </div>
+    <Suspense fallback={null}>
+      <div className="flex justify-center items-center h-[calc(100vh-92px)] bg-gray-100 dark:bg-gray-900">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white dark:bg-gray-800 rounded-lg shadow-md">
+          <div className="text-center">
+            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6 text-gray-900 dark:text-white">
+              Join Stealthy Note 🥷📝
+            </h1>
+            <p className="mb-6 text-gray-700 dark:text-gray-300">
+              Sign In to start stealing notes from your friends
+            </p>
+          </div>
 
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormField
-              control={form.control}
-              name="identifier"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900 dark:text-gray-300 mt-8">
-                    Username or Email
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Username or Email"
-                      {...field}
-                      type="text"
-                      className="dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-gray-900 dark:text-gray-300 mt-8">
-                    Password
-                  </FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Password"
-                      type="password"
-                      {...field}
-                      className="dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <div className="flex justify-center w-full">
-              <Button
-                type="submit"
-                disabled={isSubmitting}
-                className="dark:bg-gray-700 dark:text-white mt-8"
-              >
-                {isSubmitting ? (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
-                    wait
-                  </>
-                ) : (
-                  "Sign In"
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="identifier"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-gray-900 dark:text-gray-300 mt-8">
+                      Username or Email
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Username or Email"
+                        {...field}
+                        type="text"
+                        className="dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
                 )}
-              </Button>
-            </div>
-          </form>
-        </Form>
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-gray-900 dark:text-gray-300 mt-8">
+                      Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        type="password"
+                        {...field}
+                        className="dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-        <div className="text-center mt-6">
-          <p className="text-gray-700 dark:text-gray-300 mt-8">
-            Forgot your password?{" "}
-            <Link
-              href={{
-                pathname: "/forgot-password",
-                query: { identifier: form.getValues("identifier") },
-              }}
-              className="text-blue-500 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300"
-            >
-              Forgot Password
-            </Link>
-          </p>
-        </div>
+              <div className="flex justify-center w-full">
+                <Button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="dark:bg-gray-700 dark:text-white mt-8"
+                >
+                  {isSubmitting ? (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please
+                      wait
+                    </>
+                  ) : (
+                    "Sign In"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
 
-        <div className="text-center mt-6">
-          <p className="text-gray-700 dark:text-gray-300 mt-8">
-            Don&apos;t have an account?{" "}
-            <Link
-              href={{
-                pathname: "/sign-up",
-                query: {
-                  identifier: form.getValues("identifier")
-                    ? form.getValues("identifier")
-                    : identifier,
-                },
-              }}
-              className="text-blue-500 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300"
-            >
-              Sign Up
-            </Link>
-          </p>
+          <div className="text-center mt-6">
+            <p className="text-gray-700 dark:text-gray-300 mt-8">
+              Forgot your password?{" "}
+              <Link
+                href={{
+                  pathname: "/forgot-password",
+                  query: { identifier: form.getValues("identifier") },
+                }}
+                className="text-blue-500 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300"
+              >
+                Forgot Password
+              </Link>
+            </p>
+          </div>
+
+          <div className="text-center mt-6">
+            <p className="text-gray-700 dark:text-gray-300 mt-8">
+              Don&apos;t have an account?{" "}
+              <Link
+                href={{
+                  pathname: "/sign-up",
+                  query: {
+                    identifier: form.getValues("identifier")
+                      ? form.getValues("identifier")
+                      : identifier,
+                  },
+                }}
+                className="text-blue-500 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-300"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
