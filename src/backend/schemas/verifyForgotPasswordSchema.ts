ca4b8f1881649b24867cef41a6d5337080f1ed54
Aman Kumar <amankarguwal0@gmail.com>
2025-02-19 23:11:00 +0530

    Add identifier and confirmPassword fields to verifyForgotPasswordSchema

--- src/backend/schemas/verifyForgotPasswordSchema.ts
+++ src/backend/schemas/verifyForgotPasswordSchema.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 import { verifyCodeValidation } from "./verifySchema";
 import { passwordValidation } from "./signUpSchema";
+import { identifierValidation } from "./signInSchema";
 
 export const verifyForgotPasswordSchema = z.object({
+  identifier: identifierValidation,
   password: passwordValidation,
+  confirmPassword: passwordValidation,
   verifyCode: verifyCodeValidation,
 });
